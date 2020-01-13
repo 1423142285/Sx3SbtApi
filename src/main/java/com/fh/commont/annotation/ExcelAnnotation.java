9f92647dc0b040403dff45e598490fd8e6471f98
1423142285 <1423142285@qq.com>
2020-01-13 20:55:37 +0800

    第二次提交 Excel导出完成

---
+++ src/main/java/com/fh/commont/annotation/ExcelAnnotation.java
@@ -0,0 +1,20 @@
+package com.fh.commont.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Component
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.TYPE})
+public @interface ExcelAnnotation {
+    //标题
+    String title() default "";
+    //sheet页名称
+    String sheetName() default "";
+    //字段
+    String column() default "";
+}
